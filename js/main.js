d9514805a73f876d4b0a205f569c83cb2fd934f9
jianxu <jianxu@lewatek.com>
2016-12-15 15:05:50 +0800

    gallery preview

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
 		$('.banner').unslider('animate:' + num);
 		$(dom.target).addClass('active').siblings().removeClass('active');
 	};
+	$(".img img").on('click', function(event) {
+		event.preventDefault();
+		var imgPath = event.target.src;
+		$(".img-preview img").attr('src', imgPath);
+		$(".img-preview").addClass('active');
+	});
+	$(".img-preview").on('click', function(event) {
+		event.preventDefault();
+		$(this).removeClass('active');
+	});
 });
